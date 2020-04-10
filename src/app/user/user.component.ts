b5c36e884e58cd085aa4ff5ef2c15f669857264a
Mamadou Billo Diallo <billodiallonet@gmail.com>
2020-04-10 08:03:41 +0300

    chaging export class

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -12,16 +12,8 @@ import { environment } from '../../environments/environment';
   styleUrls: ['./user.component.css'],
   providers:[UsersService,UserRequestService] 
 })
-export class UserComponent implements OnInit {
 
-  user:User;
-  repository:Repository[];
-
-  userName=""
-  constructor(private usersService:UserRequestService){
-  
-  }
-  submitUser(){
+submitUser(){
          this.usersService.userRequest(this.userName)
          this.usersService.repositoryrequest(this.userName)
          
