a1c4187718fe0a44faddd54f52240458f2d07a95
Mamadou Billo Diallo <billodiallonet@gmail.com>
2020-04-10 07:56:18 +0300

    modifying function ngOnInit

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -26,4 +26,13 @@ export class UserComponent implements OnInit {
          this.usersService.repositoryrequest(this.userName)
          
 }
-  
+ngOnInit() {
+    
+  this.usersService.userRequest('billodiallo')
+  this.usersService.repositoryrequest('billodiallo')
+       
+  this.user=this.usersService.user
+  this.repository=this.usersService.repository
+  console.log(this.repository)
+}  
+}
