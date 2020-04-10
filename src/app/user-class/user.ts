c9aeda771f29e681a2a3ca5ffb0362cfdcc56ebc
Mamadou Billo Diallo <billodiallonet@gmail.com>
2020-04-10 08:33:33 +0300

    user class constructor

--- src/app/user-class/user.ts
+++ src/app/user-class/user.ts
@@ -1,5 +1,3 @@
 export class User {
-    constructor(public name:string, public avatar_url:string, public location:string, public followers:number,public following:number,public public_repos:number,public html_url:string,public completeDate:Date){
-       
     }
 }
